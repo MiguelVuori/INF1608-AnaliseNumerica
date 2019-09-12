1f60b626957460f15cc80a638df14196ce99c516
MIGUEL FAGUNDES VUORI <e1710942@missouri.grad.inf.puc-rio.br>
2019-09-12 10:47:20 -0300

    atualizando

--- interp/interp.c
+++ interp/interp.c
@@ -19,21 +19,41 @@ void Chebyshev (int n, double a, double b, double* xi)
 void NewtonCoef (int n, double* xi, double (*f) (double), double* bi)
 {
 
-    for (int i = 0; i <= n; i++)
-    {
-        bi[i] = DDNewton(n,xi,f,0,i)
-    }
+    for (int i = 0; i < n; i++)
+        bi[i] = DDNewton(n, xi, f, 0, i);
 }
 
-double NewtonAval (int n, double* xi, double* bi, double x);
+double NewtonAval (int n, double* xi, double* bi, double x)
+
+{
+    double aux[n];  //armazena valores acumulados do produtorio de (x - xi) 
+    double f;  //avaliação 
+
+    for(int i = 0; i < n; i++)
+    {
+        if (i == 0)
+        {
+            aux[i] = 1;
+            f = bi[i];
+        }
+        else
+        {
+            aux[i] = aux[i-1] * (x - xi[i-1]);
+            f = f + (bi[i] * aux[i]);
+        }
+    }
+
+    return f;
+
+}
 
 /* Funções encapsuladas */
 
 double DDNewton (int n, double *xi, double (*f) (double), int i, int j)
 {
     if(i == j)
-        return f(x[i]);
+        return f(xi[i]);
     else
-        return DDNewton(n, xi, f, i+1, j) - DDNewton(n, xi, f, i,j - 1) / (xi[j] - xi[i]); 
+        return (DDNewton(n, xi, f, i+1, j) - DDNewton(n, xi, f, i,j - 1)) / (xi[j] - xi[i]); 
 
 }
--- interp/main.c
+++ interp/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <math.h>
 #include "interp.h"
+
 
 void exibeVetor(int n, double* v)
 {   
@@ -15,8 +17,14 @@ void exibeVetor(int n, double* v)
 int main (void)
 {
     double xi[6];
+    double bi[6];
+    double f;
 
     Chebyshev (6, 0, Pi/2 , xi);
+    NewtonCoef (6, xi, sin, bi);
     exibeVetor(6, xi);
+    exibeVetor(6, bi);
+    f = NewtonAval (6, xi, bi, Pi/6);
+    printf("\nValor real = %f\nValor pela interpolação = %f\n",sin(Pi/6),f);
     return 0;
 }
