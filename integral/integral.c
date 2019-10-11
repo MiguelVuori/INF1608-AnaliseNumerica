c916459e62831bd2b0f9348a32b8bbe4c3a2ccdf
MiguelVuori <miguelvuori@User-PC.localdomain>
2019-10-11 18:36:57 -0300

    add integral

--- integral/integral.c
+++ integral/integral.c
@@ -11,6 +11,9 @@ double h_otimo (double (*f) (double x), double (*fl) (double x), double x)
     /* x = 2.7 => hotimo = 10⁻5 */
     double h = 1, h_otimo = 0.1, e_min, e;
 
+    if(f == NULL || fl == NULL)
+        return 0;
+
     e_min = fabs(derivada(f,x,h_otimo) - fl(x));
 
     for (int i = 1; i < 12 ; i++)
@@ -21,9 +24,49 @@ double h_otimo (double (*f) (double x), double (*fl) (double x), double x)
         {
             e_min = e;
             h_otimo = h;
-            printf("h_otimo = %f \n",h_otimo);
         }
     }
     
     return h_otimo;
 }
+
+double richardson (double (*f) (double x), double x, double h)
+{
+
+    return ((pow(2,3) * derivada(f,x,h/2)) - derivada(f,x,h))/(pow(2,3) - 1);
+}
+
+double simpson (double (*f) (double), double a, double b, int n)
+{
+    double h = (b-a)/n,soma = 0,fesq,fdir = f(a);
+
+    for (int i = 0; i < n ; i++)
+    {
+        fesq = fdir;
+        fdir = f(a + h * (i + 1));
+        soma += (h/6) * (fesq + 4 * f(a + h * i + (h/2)) + fdir);
+    }
+    return soma;
+}
+
+double pontomedio (double (*f) (double), double a, double b, int n)
+{
+    double h = (b - a)/n,soma = 0, pmedio,xesq,xdir;
+
+    xdir = a;
+
+    for (int i = 0; i < n; i++)
+    {
+        xesq = xdir;
+        xdir = a + h * (i + 1);
+        pmedio = (xesq + xdir)/2 ;
+
+        soma += h * f(pmedio);
+    }
+    
+    return soma;
+
+}
+
+
+
--- integral/main.c
+++ integral/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "math.h"
 #include "integral.h"
-
+#define pi 3.141592653589793238462643383279502884197169399375105820974944592307816406286
 double dervAnal(double x)
 {
     return -sin(x) - (2 * cos(x));
@@ -12,9 +12,57 @@ double f(double x)
     return cos(x) - (2 * sin(x));
 }
 
+double f1(double x)
+{
+    return (x/(sqrt(pow(x,2) + 9)));
+}
+
+double f2(double x)
+{
+    return x * x * log(x);
+}
+
+double f3(double x)
+{
+    return x * x * sin(x);
+}
+
+
 int main ()
 {
-    printf("h_otimo = %f\n",h_otimo(f,dervAnal,2.7));
+    printf(" Teste das derivadas\n\n");
+
+    printf("h_otimo  avaliado pelo metodo= %.12f\n",h_otimo(f,dervAnal,2.7));
+    printf("h_otimo  teorico = 0.00001\n");
+    printf("derivada usando h qualquer= %.12f\n",derivada(f,2.7,0.1));
+    printf("derivada usando hOtimo= %.12f\n",derivada(f,2.7,0.00001));
+    printf("derivada analitica= %.12f\n",dervAnal(2.7));
+
+    printf("\n\n");
+
+    printf("metodo Richardson= %.12f\n",richardson(f,2.7,0.00001));
+    printf("metodo numerico= %.12f\n",derivada(f,2.7,0.00001));
+    printf("derivada analitica= %.12f\n",dervAnal(2.7));
+    printf("erro1 (richardson)= %.12f\n",fabs(dervAnal(2.7) - richardson(f,2.7,0.00001)));
+    printf("erro2 (derivada numerica)= %.12f\n",fabs(dervAnal(2.7) - derivada(f,2.7,0.00001)));
+
+    printf("\n\n Teste das integrais \n\n");
+
+    printf("integral por simpson para f1 com 16 passos = %.15f \n", simpson(f1,0,4,16));
+    printf("integral por simpson para f1 com 32 passos = %.15f \n", simpson(f1,0,4,32));
+    printf("integral por simpson para f2 com 16 passos = %.15f \n", simpson(f2,1,3,16));
+    printf("integral por simpson para f2 com 32 passos = %.15f \n", simpson(f2,1,3,32));
+    printf("integral por simpson para f3 com 16 passos = %.15f \n", simpson(f3,0,pi,16));
+    printf("integral por simpson para f3 com 32 passos = %.15f \n", simpson(f3,0,pi,32));
+
+    printf("integral por ponto medio para f1 com 16 passos = %.15f \n", pontomedio(f1,0,4,16));
+    printf("integral por ponto medio para f1 com 32 passos = %.15f \n", pontomedio(f1,0,4,32));
+    printf("integral por ponto medio para f2 com 16 passos = %.15f \n", pontomedio(f2,1,3,16));
+    printf("integral por ponto medio para f2 com 32 passos = %.15f \n", pontomedio(f2,1,3,32));
+    printf("integral por ponto medio para f3 com 16 passos = %.15f \n", pontomedio(f3,0,pi,16));
+    printf("integral por ponto medio para f3 com 32 passos = %.15f \n", pontomedio(f3,0,pi,32));
+
+    printf("O metodo simpson apresenta melhor precisao e o aumento do numero de passos aumenta a precisao");
 
     return 0;
 }
