39a0d738301c2c295d90163e35e7763efc6de253
miguelvuori <mfvuori@gmail.com>
2019-10-18 21:13:29 -0300

    add simpson

--- simpson/main.c
+++ simpson/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <math.h>
-#include "simpson.h" 
+#include "simpson.h"
+#define Pi 3.141592653589793238462643383279502884197169399375105820974944592307816406286
 
 double f1(double x)
 {
@@ -26,26 +27,20 @@ int main ()
     printf("Valor da integral = %.12f\n\n",v);
 
     printf("Teste f2\n");
-    printf("Erro simpson = %.12f\n",DoubleSimpson(0,1,f2,&v));
-    printf("Valor da integral = %.12f\n\n",v);
+    printf("Erro simpson = %.12f\n",DoubleSimpson(-1,1,f2,&v));
+    printf("Valor da integral = %.12f\n\n",(1/(sqrt(2 * Pi))) * v);
 
     printf("Teste f3\n");
-    printf("Erro simpson = %.12f\n",DoubleSimpson(0,1,f3,&v));
-    printf("Valor da integral = %.12f\n\n",v);
+    printf("Erro simpson = %.12f\n",DoubleSimpson(0,3,f3,&v));
+    printf("Valor da integral = %.12f\n\n",(2/sqrt(Pi))* v);
 
     printf("Teste Simpson adaptativo\n");
     printf("Teste f1\n");
-    printf("Erro simpson adaptativo = %.12f\n",AdaptiveSimpson(0,1,f1,0.0000016));
-    //printf("Valor da integral = %.12f\n\n",v);
+    printf("Simpson adaptativo = %.12f\n",AdaptiveSimpson(0,1,f1,0.0000016));
 
-    printf("Teste f2\n");
-    printf("Erro simpson adaptativo = %.12f\n",AdaptiveSimpson(0,1,f2,0.000029));
-    //printf("Valor da integral = %.12f\n\n",v);
-
-    printf("Teste f3\n");
-    printf("Erro simpson adaptativo = %.12f\n",AdaptiveSimpson(0,1,f3,0.000021));
-    //printf("Valor da integral = %.12f\n\n",v);
-
+    printf("\nTeste Quadratura de Gauss adaptativo\n");
+    printf("Quadratura2 de f1= %.12f\n",Quadratura2 ( 0, 1, f1 ));
+    printf("Quadratura3 de f1= %.12f\n",Quadratura3 ( 0, 1, f1 ));
 
 
     return 0;
--- simpson/simpson.c
+++ simpson/simpson.c
@@ -2,6 +2,13 @@
 #include <math.h>
 #include "simpson.h"
 
+
+/* Protótipo das funções encapsuladas pelo módulo */
+
+static double TransformaQuadratura( double a, double b, double t, double (*f) (double x) );
+
+/* Funções exportadas pelo módulo */
+
 double DoubleSimpson (double a, double b, double (*f) (double x), double* v)
 {
     double c = (a + b)/2, h = c - a, S2,S1,S0;
@@ -22,4 +29,48 @@ double AdaptiveSimpson (double a, double b, double (*f) (double x), double tol)
         return v;
 
     return AdaptiveSimpson (a,(a+b)/2, f, tol/2) + AdaptiveSimpson ((a+b)/2, b, f, tol/2);
+}
+
+double Quadratura2 ( double a , double b , double (*f) (double x) )
+{
+   double x[2] = { - sqrt((double)1/3) , 
+                     sqrt((double)1/3) };
+
+   double c[2] = { 1 , 1 };
+   double soma = 0;
+
+   int i;
+
+   for( i = 0 ; i < 2 ; i++ )
+      soma += c[i]*TransformaQuadratura(a,b,x[i],f);
+
+   return soma;
+}
+
+/*
+------------------------------------------
+QUADRATURA COM 3 AMOSTRAS
+------------------------------------------
+*/
+
+double Quadratura3 ( double a , double b , double (*f) (double x) )
+{
+   double x[3] = { - sqrt((double)3/5) , 0 , sqrt((double)3/5) };
+
+   double c[3] = { (double)5/9 , (double)8/9 , (double)5/9 };
+   double soma = 0;
+
+   int i;
+
+   for( i = 0 ; i < 3 ; i++ )
+      soma += c[i]*TransformaQuadratura(a,b,x[i],f);
+
+   return soma;
+}
+
+/* Corpo das funções encapsuladas pelo módulo */
+
+double TransformaQuadratura( double a , double b , double t , double (*f) (double x) )
+{
+   return (*f)(( (b-a)*t + (b+a) )/2) * (b-a)/2;
 }
